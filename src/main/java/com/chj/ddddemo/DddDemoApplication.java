25649bfee3ae6acbde25b4e69dd3290ad5ca55e8
chj3737 <chenhj86@gmail.com>
2018-03-22 12:57:27 +0800

    Initial commit

---
+++ src/main/java/com/chj/ddddemo/DddDemoApplication.java
@@ -0,0 +1,12 @@
+package com.chj.ddddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddDemoApplication.class, args);
+	}
+}
